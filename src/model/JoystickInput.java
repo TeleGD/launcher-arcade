bd7efada772549605c5328ddeaa6412472f37960
stephane-hulot <hulot.stephane.55@gmail.com>
2020-02-06 21:26:38 +0100

    Ajout de test manette

--- src/model/JoystickInput.java
+++ src/model/JoystickInput.java
@@ -4,28 +4,76 @@ import net.java.games.input.Component;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
-import java.io.File;
-
 public class JoystickInput {
+    private Controller gamepad;
+    private Component[] components;
+    private int axisCount;
+
+    public JoystickInput() {
+        Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        gamepad = null;
+        System.out.println("Controleurs : ");
+        for(int i = 0; i < controllers.length; i++) {
+            System.out.println(controllers[i].getName());
+            if(controllers[i].getType() == Controller.Type.GAMEPAD)
+                gamepad = controllers[i];
+        }
+
+        axisCount = 0;
+        if(gamepad != null) {
+            pollGamepad();
+            for (int i = 0; i < getComponentCount(); i++) {
+                if(components[i].isAnalog())
+                    axisCount = i;
+            }
+        }
+    }
+
+    public boolean isGamepadConnected() {
+        return gamepad != null;
+    }
+
+    public void pollGamepad() {
+        if(isGamepadConnected()) {
+            gamepad.poll();
+            components = gamepad.getComponents();
+        }
+    }
+
+    public int getComponentCount() {
+        return components.length;
+    }
+
+    public int getAxisCount() {
+        return axisCount;
+    }
+
+    public float getComponentValue(int index) {
+        if(index > getComponentCount())
+            return 0;
+
+        return components[index].getPollData();
+    }
+
+    // TEST
+
     public static void main(String[] args) {
-        System.setProperty("net.java.games.input.librarypath", new File("sys").getAbsolutePath());
-        Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        JoystickInput test = new JoystickInput();
 
-        for(int i =0 ; i<ca.length; i++) {
-            if (ca[i].getType() == Controller.Type.GAMEPAD) {
-                while (true) {
-                    Component[] components = ca[i].getComponents();
-                    for (int j = 0; j < components.length; j++) {
-                        if (components[i].isAnalog()) {
-                            System.out.println(components[i].getPollData());
-                        }
+        if(test.isGamepadConnected()) {
+            while (true) {
+                test.pollGamepad();
 
-                        try {
-                            Thread.sleep(20);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                for (int i = 0; i < test.getComponentCount(); i++) {
+                    float val = Math.round(test.getComponentValue(i) * 10f) / 10f;
+                    System.out.print(i + (val < 0 ? ":" : ": ") + val + " ; ");
+                }
+                System.out.println();
+
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
